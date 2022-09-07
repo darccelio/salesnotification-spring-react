e2deb0c87d3a7fdba7f0bf126fa27b601b52d0cc
Darccelio Silva <darccelio@gmail.com>
2022-09-07 19:19:48 -0300

    feat: add lib react-datepicker@4.8.0 @types/react-datepicker@4.4.2 to SalesCard componenet

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,5 +1,8 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import NotificationButton from "../NotificationButton";
-import "./styles.css"
+import "./styles.css";
 
 function SalesCard() {
 	return (
@@ -8,10 +11,20 @@ function SalesCard() {
 				<h2 className="dsmeta-sales-title">Vendas</h2>
 				<div>
 					<div className="dsmeta-form-control-container">
-						<input className="dsmeta-form-control" type="text" />
+						<DatePicker
+							selected={new Date()}
+							onChange={(date: Date) => {}}
+							className="dsmeta-form-control"
+							dateFormat="dd/MM/yyyy"
+						/>
 					</div>
 					<div className="dsmeta-form-control-container">
-						<input className="dsmeta-form-control" type="text" />
+						<DatePicker
+							selected={new Date()}
+							onChange={(date: Date) => {}}
+							className="dsmeta-form-control"
+							dateFormat="dd/MM/yyyy"
+						/>
 					</div>
 				</div>
 
@@ -64,7 +77,7 @@ function SalesCard() {
 								<td>R$ 55300.00</td>
 								<td>
 									<div className="dsmeta-red-btn-container">
-                    <NotificationButton />
+										<NotificationButton />
 									</div>
 								</td>
 							</tr>
