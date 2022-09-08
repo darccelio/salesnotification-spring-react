48f3749e78f3bb9d9a766efeac9e20ba88f95ef7
Darccelio Silva <darccelio@gmail.com>
2022-09-08 11:53:51 -0300

    feat: SalesController with endpoint FindSales(GET) and test ok with postman tool

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,2 +1,23 @@
-package com.devsuperior.dsmeta.controllers;public class SaleController {
+package com.devsuperior.dsmeta.controllers;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService saleService;
+
+    @GetMapping
+    public List<Sale> findSales() {
+        return saleService.findSales();
+    }
 }
