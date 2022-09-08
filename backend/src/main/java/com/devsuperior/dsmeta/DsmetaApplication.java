c6af769e9901303abab2fd926eb39e59a9b35a80
Darccelio Silva <darccelio@gmail.com>
2022-09-08 11:14:10 -0300

    feat: config JPA Repository and H2 database with Sales entitiy

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsmeta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DsmetaApplication {
 
--- backend/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
@@ -1,2 +1,7 @@
-package com.devsuperior.dsmeta.repository;public interface SaleRepository {
+package com.devsuperior.dsmeta.repository;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 }
